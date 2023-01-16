ab40d0deac86357fcf7a17f9d3f98559083a39f2
patelheli <pahelipi@shernet.sheridancollege.ca>
2023-01-16 11:49:06 -0500

    student project version 1

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author patel
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student s1= new Student();
+        s1.setName("peter");
+        s1.setAge(23);
+        Student s2= new Student();
+        s2.setName("ranbir");
+        s2.setAge(23);
+        Student s3= new Student();
+        s3.setName("stuti");
+        s3.setAge(23);
+        list[0] = s1;
+        list[1] = s2;
+        list[2] = s3;
+    }
+    
+}
